41fb4bee6f35dc74b627bbbdd7faafc5453f734e
atsewkaleb <atsewkaleb@gmail.com>
2023-11-30 05:16:32 +1100

    adding

---
+++ numberOfEmployeesWhoMetTheTarget.cpp
@@ -0,0 +1,10 @@
+class Solution {
+ public:
+  int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+    return ranges::count_if(hours,[target](int hour) 
+       { 
+        return hour >= target; 
+        }
+        );
+  }
+};
